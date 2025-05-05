a0a7015a4c17a5ba3b107100c53489342ed07e6b
kdeano <kyle.antony.dean@gmail.com>
2025-05-06 00:34:55 +0100

    feat: initial commit

---
+++ client/src/app/(nondashboard)/landing/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Hero from "./hero";
+import Features from "./features";
+import Discover from "./discover";
+
+const Landing = () => {
+  return (
+    <div>
+      <Hero />
+      <Features />
+      <Discover />
+    </div>
+  );
+};
+
+export default Landing;
