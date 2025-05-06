9ebdd8b7a7132535ad2f9407a9eb7a38f8e87de9
kdeano <kyle.antony.dean@gmail.com>
2025-05-06 20:13:34 +0100

    feat: sidebar

---
+++ client/src/app/(dashboard)/layout.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import Navbar from "@/components/navbar";
+import { SidebarProvider } from "@/components/ui/sidebar";
+import Sidebar from "@/components/app-sidebar";
+import { NAVBAR_HEIGHT } from "@/lib/constants";
+import React from "react";
+import { useGetAuthUserQuery } from "@/state/api";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+    const { data: authUser } = useGetAuthUserQuery();
+
+    if (!authUser?.userRole) return null;
+
+    return (
+        <SidebarProvider>
+            <div className="min-h-screen w-full bg-primary-100">
+                <Navbar />
+                <div style={{ paddingTop: `${NAVBAR_HEIGHT}px` }}>
+                    <main className="flex">
+                        <Sidebar userType={authUser.userRole.toLowerCase()} />
+                        <div className="flex-grow transition-all duration-300">
+                            {children}
+                        </div>
+                    </main>
+                </div>
+            </div>
+        </SidebarProvider>
+    );
+};
+
+export default Layout;
