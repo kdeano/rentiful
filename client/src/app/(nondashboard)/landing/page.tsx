68be37cf9c60c3cc5b2fadd2277b82434eb4ea94
kdeano <kyle.antony.dean@gmail.com>
2025-05-06 12:25:33 +0100

    feat: landing page frontend

--- client/src/app/(nondashboard)/landing/page.tsx
+++ client/src/app/(nondashboard)/landing/page.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import Hero from "./hero";
 import Features from "./features";
 import Discover from "./discover";
+import CallToAction from "./cta";
+import Footer from "./footer";
 
 const Landing = () => {
   return (
@@ -9,6 +11,8 @@ const Landing = () => {
       <Hero />
       <Features />
       <Discover />
+      <CallToAction />
+      <Footer />
     </div>
   );
 };
