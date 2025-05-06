949f8ce626d3efc54e0aff67f5f2d7b7d4b7df97
kdeano <kyle.antony.dean@gmail.com>
2025-05-06 19:46:56 +0100

    chore: sidebar trigger

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -14,6 +14,7 @@ import {
     DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { SidebarTrigger } from "./ui/sidebar";
 
 const Navbar = () => {
     const { data: authUser } = useGetAuthUserQuery();
@@ -32,6 +33,11 @@ const Navbar = () => {
         <div className="fixed top-0 left-0 w-full z-50 shadow-xl">
             <div className="flex justify-between items-center w-full py-3 px-8 bg-primary-700 text-white">
                 <div className="flex items-center gap-4 md:gap-6">
+                    {isDashboardPage && (
+                        <div className="md:hidden">
+                            <SidebarTrigger />
+                        </div>
+                    )}
                     <Link
                         href="/"
                         className="cursor-pointer hover:!text-primary-300"
